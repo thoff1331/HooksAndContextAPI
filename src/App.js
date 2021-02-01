80da6ecc8922faba91272358f9f39317c991bccd
Trevor hoffman <trev@Trevors-MacBook-Air-2.local>
2021-01-31 22:10:48 -0700

    add dark mode capability

--- src/App.js
+++ src/App.js
@@ -3,19 +3,28 @@ import React, { useState,useEffect } from 'react';
 
 function App() {
  const [clickCount,setClickCount] = useState(0) 
+ const [darkTheme,setdarkTheme] =  useState(false)
  useEffect(() => {
    if(clickCount <= 0) {
+
      setClickCount(0)
+     document.title = `${clickCount}`
+
    } else {
 document.title = `${clickCount}`
    }
 
  },[clickCount])
   return (
-    <div className="App">
+    <div className={`${darkTheme}`}>
+      <div className='element-container'>
       <button onClick={() => setClickCount(clickCount + 1)}>Add Click</button>
   <p>You've Clicked {clickCount} Times</p>
   <button onClick={() => setClickCount(clickCount - 1)}>Remove click</button>
+  <div>
+  </div>  
+    <button onClick={() => setdarkTheme( darkTheme => !darkTheme)}>{darkTheme ? "Dark Mode" : "Light Mode"}</button>
+  </div>
     </div>
   );
 }
