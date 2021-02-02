2e879e4e0379829165d857df205868bc28f7fcb7
Trevor hoffman <trev@Trevors-MacBook-Air-2.local>
2021-02-02 12:09:29 -0700

    added todo list and remove ite m

--- src/components/Todo/todo.js
+++ src/components/Todo/todo.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 function Todo() {
+ const [list,setList] = useState([])
+ const [newItem,setNewItem] = useState('')
+ console.log(list,newItem)   
     return (
-        <h1>Todo List</h1>
+       <div>
+        <input onChange={(e) => setNewItem(e.target.value)  } placeholder='Add a To do List Item' />
+        <button onClick={() => setList([...list,newItem])}>Add Task</button>   
+        <div>
+          { list && list.map((el,index) => {
+        return  <h1 onClick={() => setList(list.filter(item => item != el)) }>{el}</h1>    
+          })}  
+        </div>
+       </div>
     )
 }
 
